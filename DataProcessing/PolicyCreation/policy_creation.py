0f8becb1723591ff992f128389a1ef21390dd5a9
Atyab <hakeematyab7@gmail.com>
2025-02-11 14:12:34 -0500

    added generate policy and pdf2text function

--- DataProcessing/PolicyCreation/policy_creation.py
+++ DataProcessing/PolicyCreation/policy_creation.py
@@ -1,10 +1,18 @@
+import os
+import shutil
+import json
+
 from PyPDF2 import PdfReader, PdfWriter
+import fitz
+import openai
 
 def chunk_pdf(input_pdf_path, output_dir, chunk_size=50):
     reader = PdfReader(input_pdf_path)
     total_pages = len(reader.pages)
     
     chunks = []
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     for i in range(0, total_pages, chunk_size):
         writer = PdfWriter()
         chunk_pages = reader.pages[i:i + chunk_size]
@@ -22,19 +30,39 @@ def download_standards():
     standards_path = './docs/auditing_standards_audits_fybeginning_on_or_after_december_15_2024.pdf'
     return standards_path
 
-def pdf2text(pdf):
-    pass
+def pdf2text(pdf_path):
+    doc = fitz.open(pdf_path)
+    text = ""
+    for page in doc:
+        text+= page.get_text("text") + "\n"
+    return text.strip()
 
-def generate_policy(pdfs, prompt,model='gpt-4o'):
-    pass
+def generate_rules(prompt, text, model='gpt-4o'):
+    rules = []
+    return rules
+
+def generate_policy(pdfs, prompt, output_path, model):
+    policy = []
+    for pdf in pdfs:
+        text = pdf2text(pdf)
+        rules = generate_rules(prompt, text, model)
+        policy.extend(rules)
+    shutil.rmtree(os.path.dirname(pdfs[0]))
+    with open(os.path.join(output_path, 'rules.json'),'w', encoding='utf-8') as f:
+        json.dump(policy, f, indent=4)
+
 
 if __name__=='__main__':
     prompt_path = './docs/prompt.txt'
+    output_path = './docs'
     temp_path = './temp'
     model = 'gpt-4o'
-    standards_path = download_standards()
-    with open(prompt_path,'r') as f:
-        prompt = f.read()
-    chunk_size = 50
-    chunks = chunk_pdf(standards_path, temp_path, chunk_size)
-    generate_policy(chunks, prompt, model)
+    try:
+        standards_path = download_standards()
+        with open(prompt_path,'r') as f:
+            prompt = f.read()
+        chunk_size = 50
+        chunks = chunk_pdf(standards_path, temp_path, chunk_size)
+        generate_policy(chunks, prompt, output_path, model)
+    except Exception as e:
+        print(f'Policy Generation Failed\nDetails: {str(e)}')
