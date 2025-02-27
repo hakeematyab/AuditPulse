185876968293b3f8bc4f53163da81deac6a22c7e
Atyab <hakeematyab7@gmail.com>
2025-02-26 19:31:45 -0500

    added save policy test

--- DataProcessing/PolicyCreation/test_policy_creation.py
+++ DataProcessing/PolicyCreation/test_policy_creation.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch, mock_open
 import policy_creation
 
 class TestPolicyCreation(unittest.TestCase):
@@ -8,6 +8,19 @@ class TestPolicyCreation(unittest.TestCase):
     def setUp(self):
         self.collection_name = 'config'
         self.document_name = 'policy'
+        self.policy = [
+                        {
+                            "rule_id": "PCAOB-2401",
+                            "standard": "AS 2401 - Consideration of Fraud in a Financial Statement Audit",
+                            "description": "Auditors must exercise professional skepticism to detect and respond to fraud risks in financial statements.",
+                            "enforcement_guidelines": [
+                                "Identify and assess fraud risks, including the risk of management override of controls.",
+                                "Implement audit procedures to address fraud risks and verify financial accuracy.",
+                                "Communicate fraud risks and findings to management, the audit committee, and regulatory authorities when necessary."
+                            ]
+                        }
+                    ]
+        self.policy_output_path = './policy_v0.json'
     
     @classmethod
     def tearDown(self):
@@ -35,13 +48,21 @@ class TestPolicyCreation(unittest.TestCase):
         with self.assertRaises(ValueError):
             policy_creation.get_document(db_client, self.collection_name, self.document_name)
 
-        
-    # chunk_pdf
-    # pdf2text
-    # generate_rules
-    # save_policy
-    # update collection
+    def test_chunk_pdf(self):
+        pass
 
+    def test_pdf2text(self):
+        pass
+
+    def test_generate_rules(self):
+        pass
+
+    def test_save_policy(self):
+        with patch('builtins.open', mock_open()) as mock_open_file,\
+        patch('json.dump') as mock_json_dump:
+            policy_creation.save_policy(self.policy, self.policy_output_path)
+            mock_open_file.assert_called_once_with(self.policy_output_path, 'w', encoding='utf-8')
+            mock_json_dump.assert_called_once_with(self.policy, mock_open_file(), indent=4)
 
 if __name__=='__main__':
     unittest.main()
